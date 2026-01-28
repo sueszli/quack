63b28cb47273fdb141c19b6bcaf58984a2043462
apirrone <awanegenebayo@gmail.com>
2026-01-28 13:52:52 +0100

    imu delay max 1->2

--- src/mjlab_microduck/tasks/microduck_velocity_env_cfg.py
+++ src/mjlab_microduck/tasks/microduck_velocity_env_cfg.py
@@ -272,11 +272,11 @@ def make_microduck_velocity_env_cfg(
     )
 
     cfg.observations["policy"].terms["base_ang_vel"].delay_min_lag = 0
-    cfg.observations["policy"].terms["base_ang_vel"].delay_max_lag = 1
+    cfg.observations["policy"].terms["base_ang_vel"].delay_max_lag = 2
     cfg.observations["policy"].terms["base_ang_vel"].delay_update_period = 64
 
     cfg.observations["policy"].terms["projected_gravity"].delay_min_lag = 0
-    cfg.observations["policy"].terms["projected_gravity"].delay_max_lag = 1
+    cfg.observations["policy"].terms["projected_gravity"].delay_max_lag = 2
     cfg.observations["policy"].terms["projected_gravity"].delay_update_period = 64
 
     # Add imitation observations if using imitation
