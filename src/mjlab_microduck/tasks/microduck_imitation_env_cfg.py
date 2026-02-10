154fee9a949b11c674279c9ef9fd906eaa834dda
apirrone <awanegenebayo@gmail.com>
2026-02-10 13:22:09 +0100

    more projected gravity delay

--- src/mjlab_microduck/tasks/microduck_imitation_env_cfg.py
+++ src/mjlab_microduck/tasks/microduck_imitation_env_cfg.py
@@ -307,8 +307,8 @@ def make_microduck_imitation_env_cfg(play: bool = False, ghost_vis: bool = False
         cfg.observations["policy"].terms["base_ang_vel"].delay_update_period = 64
         cfg.observations["policy"].terms["base_ang_vel"].noise = Unoise(n_min=-0.4, n_max=0.4)
 
-        cfg.observations["policy"].terms["projected_gravity"].delay_min_lag = 0
-        cfg.observations["policy"].terms["projected_gravity"].delay_max_lag = 3  # 40-120ms at 50Hz
+        cfg.observations["policy"].terms["projected_gravity"].delay_min_lag = 2
+        cfg.observations["policy"].terms["projected_gravity"].delay_max_lag = 6
         cfg.observations["policy"].terms["projected_gravity"].delay_update_period = 64
         cfg.observations["policy"].terms["projected_gravity"].noise = Unoise(n_min=-0.15, n_max=0.15)
 
