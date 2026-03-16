423abbcfd47bb8e36eaecf78d877015a13b0f374
apirrone <awanegenebayo@gmail.com>
2026-03-16 13:51:49 +0100

    stuff

--- src/mjlab_microduck/tasks/microduck_velocity_rollers_env_cfg.py
+++ src/mjlab_microduck/tasks/microduck_velocity_rollers_env_cfg.py
@@ -193,7 +193,10 @@ def make_microduck_velocity_rollers_env_cfg(
         func=microduck_mdp.neck_joint_pos_l2, weight=-2.0
     )
     cfg.rewards["alive"] = RewardTermCfg(
-        func=envs_rewards.is_alive, weight=5.0,
+        func=envs_rewards.is_alive, weight=15.0,
+    )
+    cfg.rewards["termination_penalty"] = RewardTermCfg(
+        func=envs_rewards.is_terminated, weight=-100.0,
     )
     cfg.rewards["joint_torques_l2"] = RewardTermCfg(
         func=microduck_mdp.joint_torques_l2, weight=-1e-3
